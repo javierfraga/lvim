dfbb6673c8fc6a871e94f7153315bcbfc98416d5
Javier Fraga <javier.s.fraga@gmail.com>
2025-03-21 21:59:28 -0500

    Moved keymaps to neovim, use them all the time

--- lua/lvim-configs/keymaps.lua
+++ lua/lvim-configs/keymaps.lua
@@ -2,12 +2,3 @@
 lvim.leader = "space"
 -- silly buffer controls to match IDEs
 -- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-vim.api.nvim_set_keymap('n', '<C-t>', ':enew<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-S-t>', ':e#<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-w>', ':x<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-b>', ':bd<CR>', { noremap = true, silent = true })
--- Make new buffer splits
-vim.api.nvim_set_keymap('n', '<C-S-h>', ':leftabove vsplit<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-S-j>', '<C-w>s<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-S-k>', ':aboveleft split<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-S-l>', '<C-w>v<CR>', { noremap = true, silent = true })
